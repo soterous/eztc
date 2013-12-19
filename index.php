9618fe90f6b300ca26e20f974ab0925ea2493308
dskinner <dskinner@caci.com>
2013-12-19 17:01:52 -0500

    Move catchall to bottom

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <?php
 require 'flight/Flight.php';
 
-Flight::route('*', function(){
-  Flight::render('home', array('page' => 'home'));
-});
+Flight::route('/submit', function($user){
+  
+}
 
 Flight::route('/user/@user', function($user){
-  Flight::render('userdetails');
+  Flight::render('userdetails');  
 });
 
 Flight::route('/project/@user', function($user){
   Flight::render('projectdetails');
 });
+
+Flight::route('*', function(){
+  Flight::render('home', array('page' => 'home'));
+});
+
 
 Flight::start();
 ?>
