c31ade7b08d29eeb5f5b6eb88f2e6b3aa213dc99
dskinner <dskinner@caci.com>
2013-12-20 15:15:00 -0500

    Add GM Script

---
+++ Greasemonkey/eztc.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        EZTC
+// @namespace   com.soterous.eztc
+// @description Pushes each DELTEK timecard to the EZTC server
+// @include     https://*/DeltekTC/TimeCollection.msv
+// @version     1.0
+// @downloadURL https://raw.github.com/soterous/EZTC/master/Greasemonkey/eztc.user.js
+// @updateURL   https://raw.github.com/soterous/EZTC/master/Greasemonkey/eztc.meta.js
+// @require  http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @grant    GM_getValue
+// @grant    GM_setValue
+// @grant    GM_listValues
+// @grant    GM_deleteValue
+// ==/UserScript==
