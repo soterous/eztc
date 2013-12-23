5af93b208e8068a0e6388b2463a819f5d12b1cec
dskinner <dskinner@caci.com>
2013-12-23 11:38:24 -0500

    Change gm script to use gm_xmlhttpRequest method

--- Greasemonkey/eztc.meta.js
+++ Greasemonkey/eztc.meta.js
@@ -7,8 +7,5 @@
 // @downloadURL https://raw.github.com/soterous/EZTC/master/Greasemonkey/eztc.user.js
 // @updateURL   https://raw.github.com/soterous/EZTC/master/Greasemonkey/eztc.meta.js
 // @require  http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
-// @grant    GM_getValue
-// @grant    GM_setValue
-// @grant    GM_listValues
-// @grant    GM_deleteValue
+// @grant    GM_xmlhttpRequest
 // ==/UserScript==
