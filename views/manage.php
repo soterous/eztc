fbd168663f848bbb8775d36d2a94deeb606a14d1
dskinner <dskinner@caci.com>
2014-03-08 17:41:20 -0500

    Add submit button to form

--- views/manage.php
+++ views/manage.php
@@ -19,36 +19,40 @@ $qGetProjects = "SELECT `Code`, `Name` FROM `Project` ORDER BY `Code`";
       <p>Assign human friendly names to project strings.</p>
     </div>  
 
-    <table class="table">
-      <tr>
-        <th>Project String</th>
-        <th>Project Name</th>    
-      </tr>
+    <form method="post">
+      <table class="table">
+        <tr>
+          <th>Project String</th>
+          <th>Project Name</th>    
+        </tr>
 
-      <?php 
-        // Get instance of statement
-        $stmt = $mysqli->stmt_init();
-        // Pull all the projects in
-        $stmt->prepare($qGetProjects) or die("Couldn't prepare project query");
-        $stmt->execute();
-        $res = $stmt->get_result() or die("Could not get results for user project query");
-        // Our data comes to us sorted by name so they're in the proper order already
-        while($row = $res->fetch_array(MYSQLI_ASSOC)) {
-          ?>
-          <tr>
-            <td>
-              <?php echo $row['Code'];?>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <input type="text" class="form-control" value="<?php echo $row['Name'];?>" />
-            </td>
-          </tr>
-          <?php
-        }
-      ?>
-    </table>
+        <?php 
+          // Get instance of statement
+          $stmt = $mysqli->stmt_init();
+          // Pull all the projects in
+          $stmt->prepare($qGetProjects) or die("Couldn't prepare project query");
+          $stmt->execute();
+          $res = $stmt->get_result() or die("Could not get results for user project query");
+          // Our data comes to us sorted by name so they're in the proper order already
+          while($row = $res->fetch_array(MYSQLI_ASSOC)) {
+            ?>
+            <tr>
+              <td>
+                <?php echo $row['Code'];?>
+              </td>
+
+              <td>
+                <input type="text" class="form-control" value="<?php echo $row['Name'];?>" />
+              </td>
+            </tr>
+            <?php
+          }
+        ?>
+        <tr>
+          <td colspan="2"><button type="submit">SAVE</button></td>
+        </tr>
+      </table>
+    </form>
 
   </div>
   
