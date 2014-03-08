e23756e9424c6f6740d3a459e87024909f7ea3a4
Kris Howard <kris@mail.usf.edu>
2014-03-08 11:44:44 -0500

    Update to include database creation as well as drop tables if exist

--- setup/MySQLStructure.sql
+++ setup/MySQLStructure.sql
@@ -1,18 +1,26 @@
--- phpMyAdmin SQL Dump
--- version 3.5.7
+- phpMyAdmin SQL Dump
+-- version 4.1.6
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 23, 2013 at 06:33 PM
--- Server version: 5.5.29
--- PHP Version: 5.4.10
+-- Generation Time: Mar 08, 2014 at 05:38 PM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.9
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `eztc`
 --
+CREATE DATABASE IF NOT EXISTS `eztc` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `eztc`;
 
 -- --------------------------------------------------------
 
@@ -20,13 +28,14 @@ SET time_zone = "+00:00";
 -- Table structure for table `Employee`
 --
 
-CREATE TABLE `Employee` (
+DROP TABLE IF EXISTS `Employee`;
+CREATE TABLE IF NOT EXISTS `Employee` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(128) NOT NULL,
   `LastUpdated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`Id`),
   UNIQUE KEY `Name` (`Name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -34,14 +43,15 @@ CREATE TABLE `Employee` (
 -- Table structure for table `Project`
 --
 
-CREATE TABLE `Project` (
+DROP TABLE IF EXISTS `Project`;
+CREATE TABLE IF NOT EXISTS `Project` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Code` varchar(32) NOT NULL,
   `Name` varchar(128) NOT NULL,
   `LastUpdated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`Id`),
   UNIQUE KEY `Code` (`Code`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -49,7 +59,8 @@ CREATE TABLE `Project` (
 -- Table structure for table `TimeEntry`
 --
 
-CREATE TABLE `TimeEntry` (
+DROP TABLE IF EXISTS `TimeEntry`;
+CREATE TABLE IF NOT EXISTS `TimeEntry` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `ProjectId` int(11) NOT NULL,
   `EmployeeId` int(11) NOT NULL,
@@ -59,15 +70,7 @@ CREATE TABLE `TimeEntry` (
   PRIMARY KEY (`Id`),
   KEY `ProjectId` (`ProjectId`),
   KEY `EmployeeId` (`EmployeeId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 ;
-
---
--- RELATIONS FOR TABLE `TimeEntry`:
---   `ProjectId`
---       `Project` -> `Id`
---   `EmployeeId`
---       `Employee` -> `Id`
---
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Constraints for dumped tables
@@ -79,3 +82,7 @@ CREATE TABLE `TimeEntry` (
 ALTER TABLE `TimeEntry`
   ADD CONSTRAINT `TimeEntry_ibfk_1` FOREIGN KEY (`ProjectId`) REFERENCES `Project` (`Id`),
   ADD CONSTRAINT `TimeEntry_ibfk_2` FOREIGN KEY (`EmployeeId`) REFERENCES `Employee` (`Id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
